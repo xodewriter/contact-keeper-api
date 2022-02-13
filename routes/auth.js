ba8f95449f5412ff0034254d97ec6a86757a9b2f
xodewriter <jlampstack@gmail.com>
2022-02-13 10:44:44 -0500

    Added logic to log in a registered user

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
+
+const User = require('../models/User');
+
+const JWT_SECRET = config.get('db.JWT_SECRET');
 
 // @route     GET /api/auth
 // @desc      Get logged in user
@@ -11,8 +19,67 @@ router.get('/', (req, res) => {
 // @route     POST /api/auth
 // @desc      Auth user & get token
 // @access    Private
-router.post('/', (req, res) => {
-	res.send('Log in user');
-});
+router.post(
+	'/',
+	body('email').isEmail().withMessage('Please use a valid email'),
+	body('password')
+		.isLength({ min: 6 })
+		.withMessage('Passwords must be 6 or more chars'),
+	async (req, res) => {
+		// Finds the validation errors in this request and wraps them in an object with handy functions
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			// Find user from MongoDB
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				return res.status(400).json({ msg: 'Invalid Credentials' });
+			}
+
+			// Check if string password matches hashed password
+			const isPasswordMatch = await bcrypt.compareSync(password, user.password);
+
+			// IF isPasswordMatch is false
+			if (!isPasswordMatch) {
+				return res.status(400).json({ msg: 'Invalid Credentials' });
+			}
+
+			// JWT Payload
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			// JWT Signature
+			await jwt.sign(
+				payload,
+				JWT_SECRET,
+				{
+					expiresIn: 360000,
+				},
+				(err, token) => {
+					// Fail
+					if (err) {
+						return res.json({ error: err.message });
+					}
+					// Pass
+					res.json({ msg: 'Logged in user', token });
+				},
+			);
+		} catch (err) {
+			// Server Error 500
+			console.log('Error:', err.message);
+			res.status(500).json({ error: err.message });
+		}
+	},
+);
 
 module.exports = router;

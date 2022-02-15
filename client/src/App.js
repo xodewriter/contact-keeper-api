c44e7609b39b6a0d2448965a1578ae586c886de9
xodewriter <jlampstack@gmail.com>
2022-02-15 08:00:40 -0500

    Installed frontend dependencies and cleaned up React

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className='App'>
+			<h1>Contact Keeper App</h1>
+			<ul class='fa-ul'>
+				<li>
+					<i class='fa-li fa fa-check-square'></i>Test font awesome
+				</li>
+				<li>
+					<i class='fa-li fa fa-spinner fa-spin'></i>Test font awesome
+				</li>
+				<li>
+					<i class='fa-li fa fa-square'></i>Test font awesome
+				</li>
+			</ul>
+		</div>
+	);
+};
 
 export default App;

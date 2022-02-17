2fd62ee5840a15ad17c11f9a3aeefe307a4eaccb
xodewriter <jlampstack@gmail.com>
2022-02-17 17:46:20 -0500

    Added react transition group animations

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,24 +1,41 @@
 import React, { Fragment, useContext } from 'react';
-import ContactItem from './ContactItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import ContactContext from '../../context/contact/contactContext';
+import ContactItem from './ContactItem';
 
 const Contacts = () => {
-	const { contacts, filtered } = useContext(ContactContext);
+	const contactContext = useContext(ContactContext);
+	const { contacts, filtered } = contactContext;
 
 	if (contacts.length === 0) {
 		return <h4>Please add a contact</h4>;
 	}
 
+	// Separate component to keep DRY for a cleaner return()
+	const getContactList = contactArray => {
+		return (
+			<TransitionGroup className='contact-list'>
+				{contactArray.map(contact => {
+					const itemRef = React.createRef();
+					return (
+						<CSSTransition
+							nodeRef={itemRef}
+							key={contact.id}
+							classNames='item'
+							timeout={667}>
+							<div ref={itemRef}>
+								<ContactItem contact={contact} />
+							</div>
+						</CSSTransition>
+					);
+				})}
+			</TransitionGroup>
+		);
+	};
+
 	return (
 		<Fragment>
-			{/* Conditionally render filtered contacts, or the full contacts list */}
-			{filtered !== null
-				? filtered.map(contact => (
-						<ContactItem key={contact.id} contact={contact} />
-				  ))
-				: contacts.map(contact => (
-						<ContactItem key={contact.id} contact={contact} />
-				  ))}
+			{filtered ? getContactList(filtered) : getContactList(contacts)}
 		</Fragment>
 	);
 };
